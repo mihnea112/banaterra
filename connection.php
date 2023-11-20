6f3e90b9a652e191ff0c46267ef4eabe6f41b489
mihnea112 <ayanna.mihnea@yahoo.com>
2023-11-20 08:41:51 +0200

    Connection to localhost

--- connection.php
+++ connection.php
@@ -1,18 +1,14 @@
 <?php
-//Connect To Database
-$hostname='213.157.175.213:/var/lib/mysql/mysql.sock';
-$username='banaterra';
-$password='SCtXEzGN';
-$dbname='banaterra';
-$sock='/var/lib/mysql/mysql.sock';
-$usertable='user';
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "banaterra";
 
-$conn = new PDO('mysql:host=213.157.175.213:3306;dbname=banaterra', $username, $password);
-// use the connection here
-$sth = $dbh->query('SELECT * FROM user');
-
-// and now we're done; close it
-$sth = null;
-$dbh = null;
-
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
 ?>
